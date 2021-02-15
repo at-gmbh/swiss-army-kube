96fafa3cc1d29ca05ee669251e2c567bc604efcd
Karl Schriek <kschriek@gmail.com>
2021-02-15 13:29:41 +0100

    remove "tags" from "data"

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,13 +1,11 @@
 
 data "aws_subnet_ids" "all" {
   vpc_id = var.vpc_id
-  tags = var.tags
 }
 
 data "aws_security_group" "default" {
   vpc_id = var.vpc_id
   name   = "default"
-  tags = var.tags
 }
 
 
